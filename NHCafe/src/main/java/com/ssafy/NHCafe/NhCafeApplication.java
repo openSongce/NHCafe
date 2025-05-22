e95fa77ac9f6d325687fca0b6a36fd0d77ddc9d2
openSongce <rkddkwl059@naver.com>
2025-05-22 21:35:48 +0900

    test

---
+++ NHCafe/src/main/java/com/ssafy/NHCafe/NhCafeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.NHCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhCafeApplication.class, args);
+	}
+
+}
---
+++ NHCafe/src/test/java/com/ssafy/NHCafe/NhCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.NHCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
