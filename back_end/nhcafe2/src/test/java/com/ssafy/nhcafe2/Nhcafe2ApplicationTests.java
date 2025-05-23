280b103bbf5e29a984c1aa8d709a1c4740dd1b12
eunsolhh <eunsolhh@gmail.com>
2025-05-23 17:18:11 +0900

    프로젝트생성

---
+++ back_end/nhcafe2/src/test/java/com/ssafy/nhcafe2/Nhcafe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.nhcafe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhcafe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
